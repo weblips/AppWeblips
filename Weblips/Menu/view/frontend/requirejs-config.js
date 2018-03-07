36fc8bf3e5be0f63dcff843f206c2bc219fe6f20
WebLips <weblips@gmail.com>
2018-03-07 11:11:28 +0200

    add require js

---
+++ Weblips/Menu/view/frontend/requirejs-config.js
@@ -0,0 +1,14 @@
+var config = {
+    'map' : {
+        '*' : {
+            'mage/validation' : 'Weblips_Menu/js/validation'
+        }
+    },
+    config : {
+        mixins : {
+            'Weblips_Menu/js/validation' : {
+                'Weblips_Menu/js/validation-mixin': true
+            }
+        }
+    }
+};
