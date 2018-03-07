2866a787370d9ca47eb8ca78e9e6225d84e590ff
WebLips <weblips@gmail.com>
2018-03-07 10:15:31 +0200

    closure call to bind for php5.6

--- Weblips/Menu/Plugin/Logger.php
+++ Weblips/Menu/Plugin/Logger.php
@@ -1,15 +1,38 @@
 <?php
 
-/*
- *  Weblips co
- */
+namespace Weblips\Menu\Plugin;
 
-/**
- * Description of Logger
- *
- * @author uasynzheryanua
- */
+use Weblips\Menu\Console\Command\AddItemCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 class Logger
 {
-    //put your code here
+
+    private $output;
+
+    public function beforeRun(
+    AddItemCommand $command, InputInterface $input, OutputInterface $output
+    )
+    {
+        $output->writeln('work before execute');
+    }
+
+    public function aroundRun(
+    AddItemCommand $command, \Closure $proceed, InputInterface $input, OutputInterface $output
+    )
+    {
+        $output->writeln('beafore stack -Around');
+        //for PHP7
+        //$proceed->call($command, $input, $output);
+        $proceed->bind($proceed, $input, $output);
+        $output->writeln('work around -After- execute');
+        $this->output = $output;
+    }
+
+    public function afterRun(AddItemCommand $command )
+    {
+        $this->output->writeln('work after ---Last execute');
+    }
+
 }
